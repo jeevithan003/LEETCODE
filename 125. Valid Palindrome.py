b048530b0ce0e379d57d1ff3791003af961872c8
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-06 22:21:01 +0530

    Create 125. Valid Palindrome.py

---
+++ 125. Valid Palindrome.py
@@ -0,0 +1,21 @@
+# Example 1:
+
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+# Example 2:
+
+# Input: s = "race a car"
+# Output: false
+# Explanation: "raceacar" is not a palindrome.
+# Example 3:
+
+# Input: s = " "
+# Output: true
+# Explanation: s is an empty string "" after removing non-alphanumeric characters.
+# Since an empty string reads the same forward and backward, it is a palindrome.
+ class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        st=""
+        cleaned_str = st.join(filter(str.isalnum, s)).lower()
+        return cleaned_str == cleaned_str[::-1]
